dcb1974c54af44b090c43da1529cde0be28a40fe
Hemanth K <hemanthkasoju@gmail.com>
2020-02-19 11:15:33 -0600

    Initial commit

---
+++ AddQuestions/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class Question(models.Model):
+    question=models.TextField()
+    marks=models.IntegerField()
+    difficulty=models.IntegerField()
+    isImportant=models.BooleanField(default=False)
+    chapter=models.IntegerField()
+    time=models.IntegerField()
+
+# Create your models here.
